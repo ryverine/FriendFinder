9a4fe37707633b8546e03a33299a103f48c7d590
ryverine <rwcheek@gmail.com>
2019-06-12 22:27:55 -0400

    API POST is working now.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,8 @@ var router = express.Router();
 // READ THIS:
 // https://expressjs.com/en/guide/routing.html
 
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
 //var app = express();
 
@@ -38,6 +40,37 @@ router.post("/api/friends", function(req, res)
     // friends.push(newFriend);
     // res.json(newFriend);
     // console.log("apiRouts -? /api/friends -> POST");
+
+    var newFriend = req.body;
+    console.log(newFriend);
+    friends.addFriend(newFriend);
+    res.json(newFriend);
+
+/*
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+*/
+
+
+
+
+
+
 
 });
 
